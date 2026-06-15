3cb5f09e3a721236d905fa0f3d5e4243728c281d
NurKhabib <nurkhabib2006@gmail.com>
2026-06-15 11:19:16 +0300

    Completed Issue 6

--- Assets/Scripts/Betting/PlayerBetState.cs
+++ Assets/Scripts/Betting/PlayerBetState.cs
@@ -1,23 +1,39 @@
 using System;
+using System.Collections.Generic;
+
+using Player = Skeleton;
 
 [Serializable]
-public class PlayerBetState
-{
-    public bool folded;
+public class PlayerBetState {
+    public Player player;
     public DeclaredCombinationTier declaredTarget;
+    public bool hasDeclaredTarget;
+    public bool hasFolded;
+    public List<StakeAsset> committedAssets = new List<StakeAsset>();
+    public int committedValue;
+    public bool hasMatchedCurrentPrice;
 
-    public PlayerBetState()
+    public bool folded
     {
+        get => hasFolded;
+        set => hasFolded = value;
     }
 
-    public PlayerBetState(DeclaredCombinationTier declaredTarget)
-    {
-        this.declaredTarget = declaredTarget;
+    public PlayerBetState() {
     }
 
-    public PlayerBetState(DeclaredCombinationTier declaredTarget, bool folded)
-    {
+    public PlayerBetState(Player player) {
+        this.player = player;
+    }
+
+    public PlayerBetState(DeclaredCombinationTier declaredTarget) {
         this.declaredTarget = declaredTarget;
-        this.folded = folded;
+        hasDeclaredTarget = true;
+    }
+
+    public PlayerBetState(DeclaredCombinationTier declaredTarget, bool folded) {
+        this.declaredTarget = declaredTarget;
+        hasDeclaredTarget = true;
+        hasFolded = folded;
     }
 }
