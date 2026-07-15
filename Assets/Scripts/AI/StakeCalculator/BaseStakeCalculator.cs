15ad648433bf99c526ad14f39b9dd353a66c7e22
YarikYarikDaze <slava_v06@mail.ru>
2026-07-15 09:33:45 +0300

    AI stake calculator fix

--- Assets/Scripts/AI/StakeCalculator/BaseStakeCalculator.cs
+++ Assets/Scripts/AI/StakeCalculator/BaseStakeCalculator.cs
@@ -5,4 +5,6 @@ public abstract class BaseStakeCalculator
 {
     public abstract List<StakeAsset> SelectBodypartsForTradeAction(float risk, List<StakeAsset> availableAssets, int price = 0);
     protected abstract int CalculateRaisePrice(float risk, List<StakeAsset> availableAssets);
+
+    protected abstract List<StakeAsset> OrderBodyparts(List<StakeAsset> availableAssets);
 }
